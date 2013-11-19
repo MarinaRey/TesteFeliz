2910ea329045ea907fb05f35a07d0ec5b0aa8abe
Bruno Soares Fillmann <brunofillmann@hotmail.com>
2013-11-19 17:07:27 -0200

    No começo, não havia nada
    então bruno disse, que se faça o trabalho

---
+++ WindowsFormsApplication6/WindowsFormsApplication6/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Midi;
+
+namespace WindowsFormsApplication6
+{
+    
+    class Mensagem
+    {
+        //ATRIBUTOS
+        Note nota;       
+        int oitava;
+        int BPM;
+        bool repetição;
+        Mensagem próxima;
+
+        //SETS
+        void setNota(char letra)
+        {
+            nota = new Note(letra);
+        }
+        void setOitava(int novaOitava)
+        {
+            oitava = novaOitava;
+        }
+        void setBPM(int novoBPM)
+        {
+            BPM = novoBPM;
+        }
+        void setRepetição(bool repete)
+        {
+            repetição = repete;
+        }
+
+        //GETS
+        Pitch getPitch()
+        {
+            Pitch pitch;
+            pitch = nota.PitchInOctave(oitava);
+            return (pitch);
+        }
+        int getBPM()
+        {
+            return BPM;
+        }
+    }
+}
---
+++ WindowsFormsApplication6/WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Midi;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Note nota = new Note('A');
+            OutputDevice outputDevice = OutputDevice.InstalledDevices[0];
+            outputDevice.Open();
+            outputDevice.SendNoteOn(Channel.Channel1, nota.PitchInOctave(4), 80);  // Middle C, velocity 80
+            outputDevice.SendPitchBend(Channel.Channel1, 7000);  // 8192 is centered, so 7000 is bent down
+            
+
+        }
+    }
+}
